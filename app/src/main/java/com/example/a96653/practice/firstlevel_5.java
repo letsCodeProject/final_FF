e2cd0741a8378c3539ac0ddfda7a63318805cf9d
shyom <shyom1d@gmail.com>
2019-01-19 23:13:26 +0300

    hi

--- app/src/main/java/com/example/a96653/practice/firstlevel_5.java
+++ app/src/main/java/com/example/a96653/practice/firstlevel_5.java
@@ -5,7 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
-
+// i hate it too -shyom
 public class firstlevel_5 extends AppCompatActivity {
 
     @Override
