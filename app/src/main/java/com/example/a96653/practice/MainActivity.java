377c32ce2c469a62ac7c33427c36729b34425638
letsCodeProject <ree72721@gmail.com>
2019-01-20 16:56:38 +0300

    // reem change

--- app/src/main/java/com/example/a96653/practice/MainActivity.java
+++ app/src/main/java/com/example/a96653/practice/MainActivity.java
@@ -10,7 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-
+//svsvbs
 
 public class MainActivity extends AppCompatActivity {
     MyDBHandler dbHandler;
