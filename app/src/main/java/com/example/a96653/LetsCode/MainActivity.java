fc42576913991b0f0a0272dcd81b5d5f77d6e646
letsCodeProject <ree72721@gmail.com>
2019-01-24 23:34:11 +0300

    //new code

--- app/src/main/java/com/example/a96653/LetsCode/MainActivity.java
+++ app/src/main/java/com/example/a96653/LetsCode/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
                 );
     }
 
-
+////
 
     public void openPlotoActivity() {
         Intent intent = new Intent(this, firstlevel_1.class);
