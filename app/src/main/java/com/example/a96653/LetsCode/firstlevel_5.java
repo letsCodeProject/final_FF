a5e6632a5b4f75a4931078fef049b583885030db
letsCodeProject <ree72721@gmail.com>
2019-01-25 18:07:01 +0300

    //new code

--- app/src/main/java/com/example/a96653/LetsCode/firstlevel_5.java
+++ app/src/main/java/com/example/a96653/LetsCode/firstlevel_5.java
@@ -4,8 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
-// i hate it too -shyom
+
 public class firstlevel_5 extends AppCompatActivity {
 
     @Override
@@ -27,6 +28,18 @@ public class firstlevel_5 extends AppCompatActivity {
         );
 
 
+        Button  Go = (Button)findViewById(R.id.button6);
+
+        Go.setOnClickListener(
+                new Button.OnClickListener(){
+                    public void onClick(View v){
+
+                        openQuiz();
+                    }
+
+                }
+
+        );
 
 
 
@@ -40,6 +53,14 @@ public class firstlevel_5 extends AppCompatActivity {
         startActivity(intent);
 
     }
+
+    public void openQuiz(){
+
+        Intent intent=new Intent(this,firstlevel_6.class);
+        startActivity(intent);
+
+    }
+
 
 
 
