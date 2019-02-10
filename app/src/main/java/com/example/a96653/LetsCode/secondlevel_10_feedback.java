e8607d83ecef48e58d0c5bd57652e127eeb9c54f
shorog <shoshnk135@gmail.com>
2019-02-10 22:20:28 +0300

    Merge branch 'master' of https://github.com/letscodeleveltwo/letscodeLeve2
    # Conflicts:
    #	.idea/caches/gradle_models.ser
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/res/values/colors.xml
    #	app/src/main/res/values/strings.xml

---
+++ app/src/main/java/com/example/a96653/LetsCode/secondlevel_10_feedback.java
@@ -0,0 +1,41 @@
+package com.example.a96653.LetsCode;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class secondlevel_10_feedback extends AppCompatActivity {
+
+    @SuppressLint("ResourceAsColor")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secondlevel_10_feedback);
+        MySQLliteHelper m=new MySQLliteHelper(this);
+        ///to show result
+        TextView ResultBox=(TextView)findViewById(R.id.ResultBox_secondlevel_10);
+        ResultBox.setText("لبيب");
+        ResultBox.setTextColor(R.color.Shadwo_purple);
+
+
+        //TO VIEW SCORE ON BOX
+        TextView textView = (TextView)findViewById(R.id.ScoreBox_secondlevel_10);
+        textView.setText(m.getChildScore()+"" );
+
+
+        //HOME BUTTON
+        ImageButton homebtn3=(ImageButton)findViewById(R.id.homebtn_secondlevel10);
+        homebtn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent HomePage=new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(HomePage);
+            }
+        });
+
+    }
+}
