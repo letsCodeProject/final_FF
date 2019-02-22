8ed27880b39f8e79d1fd57d4d6a155d9328353a6
shorog <shoshnk135@gmail.com>
2019-02-22 19:52:01 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/a96653/LetsCode/MainActivity.java
    #	app/src/main/java/com/example/a96653/LetsCode/secondlevel_1.java
    #	app/src/main/java/com/example/a96653/LetsCode/secondlevel_14.java
    #	app/src/main/java/com/example/a96653/LetsCode/secondlevel_17.java
    #	app/src/main/java/com/example/a96653/LetsCode/secondlevel_4.java

---
+++ app/src/main/java/com/example/a96653/LetsCode/thirdlevel_6.java
@@ -0,0 +1,13 @@
+package com.example.a96653.LetsCode;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class thirdlevel_6 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thirdlevel_6);
+    }
+}
