05d91db5162ecff236cc78c002bd14c104ecf08f
shorog <shoshnk135@gmail.com>
2019-02-22 20:50:46 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/a96653/LetsCode/MainActivity.java
    #	app/src/main/java/com/example/a96653/LetsCode/secondlevel_1.java
    #	app/src/main/java/com/example/a96653/LetsCode/secondlevel_14.java
    #	app/src/main/java/com/example/a96653/LetsCode/secondlevel_17.java
    #	app/src/main/java/com/example/a96653/LetsCode/secondlevel_4.java

--- app/src/main/java/com/example/a96653/LetsCode/thirdlevel_6.java
+++ app/src/main/java/com/example/a96653/LetsCode/thirdlevel_6.java
@@ -1,7 +1,14 @@
 package com.example.a96653.LetsCode;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class thirdlevel_6 extends AppCompatActivity {
 
@@ -9,5 +16,58 @@ public class thirdlevel_6 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thirdlevel_6);
+        //NEXT BUTTON
+        final MySQLliteHelper m=new MySQLliteHelper(this);
+        ImageView goButton=(ImageView) findViewById(R.id.next_thirdlevel_6);
+
+        goButton.setOnClickListener(
+                new Button.OnClickListener(){
+                    public void onClick(View v){
+                        openSecondActivity();
+                        SharedPreferences prefs = getSharedPreferences("pref_thirdLevel_6", MODE_PRIVATE);
+                        boolean firstStart = prefs.getBoolean("firstStart", true);
+                        if (firstStart){
+                            m.UpdateNumOfLesson(11,"Nepton");//MOST BE CHANGED TO THE WRITE ONES
+                            SharedPreferences pref = getSharedPreferences("pref_thirdLevel_6", MODE_PRIVATE);
+                            SharedPreferences.Editor editor = prefs.edit();
+                            editor.putBoolean("firstStart", false);
+                            editor.apply();}
+                    }//end onClick
+
+                }//end onClickListner
+
+        );
+        //scoreBox display
+        TextView scoredisplay=(TextView) findViewById(R.id.scoreBox__thirdlevel_6);
+        scoredisplay.setText(m.getChildScore()+"" );
+
+        //HOME BUTTON
+        ImageButton homebtn9=(ImageButton)findViewById(R.id.homebtn_thirdlevel_6);
+        homebtn9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gohome=new Intent (getApplicationContext(),MainActivity.class);
+                startActivity(gohome);
+            }
+        });
+
+        ImageView previous3=(ImageView)findViewById(R.id.previous_thirdlevel_6);
+        previous3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GoToprevious();
+            }
+        });
+
+    }
+    public void openSecondActivity() {
+
+        Intent intent = new Intent(this, thirdlevel_7.class);
+        startActivity(intent);
+
+    }
+    public void GoToprevious(){
+        Intent goBack=new Intent(getApplicationContext(),thirdlevel_5.class);
+        startActivity(goBack);
     }
 }
--- app/src/main/java/com/example/a96653/LetsCode/thirdlevel_7.java
+++ app/src/main/java/com/example/a96653/LetsCode/thirdlevel_7.java
@@ -1,7 +1,14 @@
 package com.example.a96653.LetsCode;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class thirdlevel_7 extends AppCompatActivity {
 
@@ -9,5 +16,58 @@ public class thirdlevel_7 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thirdlevel_7);
+        //NEXT BUTTON
+        final MySQLliteHelper m=new MySQLliteHelper(this);
+        ImageView goButton=(ImageView) findViewById(R.id.next_thirdlevel_7);
+
+        goButton.setOnClickListener(
+                new Button.OnClickListener(){
+                    public void onClick(View v){
+                        openSecondActivity();
+                        SharedPreferences prefs = getSharedPreferences("pref_thirdLevel_7", MODE_PRIVATE);
+                        boolean firstStart = prefs.getBoolean("firstStart", true);
+                        if (firstStart){
+                            m.UpdateNumOfLesson(11,"Nepton");//MOST BE CHANGED TO THE WRITE ONES
+                            SharedPreferences pref = getSharedPreferences("pref_thirdLevel_7", MODE_PRIVATE);
+                            SharedPreferences.Editor editor = prefs.edit();
+                            editor.putBoolean("firstStart", false);
+                            editor.apply();}
+                    }//end onClick
+
+                }//end onClickListner
+
+        );
+        //scoreBox display
+        TextView scoredisplay=(TextView) findViewById(R.id.scoreBox__thirdlevel_7);
+        scoredisplay.setText(m.getChildScore()+"" );
+
+        //HOME BUTTON
+        ImageButton homebtn9=(ImageButton)findViewById(R.id.homebtn_thirdlevel_7);
+        homebtn9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gohome=new Intent (getApplicationContext(),MainActivity.class);
+                startActivity(gohome);
+            }
+        });
+
+        ImageView previous3=(ImageView)findViewById(R.id.previous_thirdlevel_7);
+        previous3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GoToprevious();
+            }
+        });
+
+    }
+    public void openSecondActivity() {
+
+        Intent intent = new Intent(this, thirdlevel_8.class);
+        startActivity(intent);
+
+    }
+    public void GoToprevious(){
+        Intent goBack=new Intent(getApplicationContext(),thirdlevel_6.class);
+        startActivity(goBack);
     }
 }
