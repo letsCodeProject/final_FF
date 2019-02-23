ba72ed8a6e98795bf56f35e94baea2f36c7e4066
shathaAM <shatha_2222@hotmail.com>
2019-02-23 19:12:36 +0300

    //thirdlevel 20

---
+++ app/src/main/java/com/example/a96653/LetsCode/thirdlevel_20_question1.java
@@ -0,0 +1,54 @@
+package com.example.a96653.LetsCode;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class thirdlevel_20_question1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thirdlevel_20_question1);
+
+//scoreBox
+        final MySQLliteHelper m=new MySQLliteHelper(this);
+
+        //scoreBox display
+        TextView scoredisplay=(TextView) findViewById(R.id.ScoreBox_thirdlevel_20);
+        scoredisplay.setText(m.getChildScore()+"" );
+
+        //HOME BUTTON
+        ImageButton homebtn6=(ImageButton)findViewById(R.id.homebtn_thirdleve20);
+        homebtn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gohome=new Intent (getApplicationContext(),MainActivity.class);
+                startActivity(gohome);
+
+            }
+        });
+
+
+        ImageView next3=(ImageView)findViewById(R.id.next_thirdlevel_20);
+        next3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GoNext();
+            }
+        });
+    }//End of Oncreate
+
+
+
+    public void GoNext(){
+        Intent gonext=new Intent(getApplicationContext(),thirdlevel_21.class);
+        startActivity(gonext);
+    }
+
+}
+
