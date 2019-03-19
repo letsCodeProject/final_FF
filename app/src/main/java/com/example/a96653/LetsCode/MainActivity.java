437dae4234896780f07db4e95917eaebe260f555
shorog <shoshnk135@gmail.com>
2019-03-19 21:25:24 +0300

    تست

--- app/src/main/java/com/example/a96653/LetsCode/MainActivity.java
+++ app/src/main/java/com/example/a96653/LetsCode/MainActivity.java
@@ -218,7 +218,7 @@ public class MainActivity extends AppCompatActivity {
        TextView t=(TextView)findViewById(R.id.textView47);
         Lesson l=new Lesson();
         String name=l.getName(M.getNumOfLessonPassed("Ploto"));
-       // t.setText(name);
+       t.setText(name);
         Class c;
         try{
         c = Class.forName("com.example.a96653.LetsCode."+name);
@@ -244,7 +244,7 @@ public class MainActivity extends AppCompatActivity {
         TextView t=(TextView)findViewById(R.id.textView47);
         Lesson l=new Lesson();
         String name=l.getName(M.getNumOfLessonPassed("Nepton"));
-        //t.setText(name);
+        t.setText(name);
         Class c;
         try{
             c = Class.forName("com.example.a96653.LetsCode."+name);
@@ -269,7 +269,7 @@ public class MainActivity extends AppCompatActivity {
         TextView t=(TextView)findViewById(R.id.textView47);
         Lesson l=new Lesson();
         String name=l.getName(M.getNumOfLessonPassed("Saturn"));
-       // t.setText(name);
+       t.setText(name);
         Class c;
         try{
             c = Class.forName("com.example.a96653.LetsCode."+name);
@@ -296,7 +296,7 @@ public class MainActivity extends AppCompatActivity {
 
         Lesson l=new Lesson();
         String name=l.getName(M.getNumOfLessonPassed("Jupiter"));
-        //t.setText(name);
+        t.setText(name);
         Class c;
         try{
             c = Class.forName("com.example.a96653.LetsCode."+name);
